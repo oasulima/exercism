20dd92b481db91de49bb3b9388df5b3a6c684c46
Oleg Sulima <oleg.sulima@outlook.com>
2023-11-07 01:21:15 +0300

    Rust: Parallel Letter Frequency [refactored]

--- rust/parallel-letter-frequency/src/lib.rs
+++ rust/parallel-letter-frequency/src/lib.rs
@@ -4,48 +4,48 @@ use std::{
 };
 
 pub fn frequency(input: &[&'static str], worker_count: usize) -> HashMap<char, usize> {
+    match input.len() {
+        0 => HashMap::new(),
+        n if n < 700 => count_chars(input),
+        _ => count_in_parallel(input, worker_count),
+    }
+}
+
+fn count_in_parallel(input: &[&str], worker_count: usize) -> HashMap<char, usize> {
     let mut result: HashMap<char, usize> = HashMap::new();
-    let mut handlers = vec![];
 
-    for chunk in input.chunks(worker_count) {
-        let chunk = chunk.to_owned();
-        let handler = thread::spawn(move || {
-            let mut letters: Box<HashMap<char, usize>> = Box::new(HashMap::new());
-            for str in chunk.iter() {
-                for char in str.chars() {
-                    if !char.is_alphabetic() {
-                        continue;
-                    }
+    thread::scope(|s| {
+        let mut handlers = Vec::with_capacity(worker_count);
+        for chunk in input.chunks(input.len() / worker_count + 1) {
+            let handler = s.spawn(|| count_chars(chunk));
+            handlers.push(handler);
+        }
 
-                    let char = char.to_ascii_lowercase();
-
-                    let counter = letters.get_mut(&char);
-                    if let Some(counter) = counter {
-                        *counter += 1;
-                    } else {
-                        letters.insert(char, 1);
-                    }
-                }
-            }
-            letters
-        });
-        handlers.push(handler);
-    }
-    for handler in handlers {
-        let data = handler.join().unwrap();
-        merge_counters(&mut result, &data);
-    }
+        for handler in handlers {
+            let data = handler.join().unwrap();
+            merge_counters(&mut result, &data);
+        }
+    });
 
     result
 }
 
+fn count_chars(input: &[&str]) -> HashMap<char, usize> {
+    let mut letters: HashMap<char, usize> = HashMap::new();
+    for str in input.iter() {
+        for char in str
+            .chars()
+            .filter(|c| c.is_alphabetic())
+            .map(|c| c.to_ascii_lowercase())
+        {
+            *letters.entry(char).or_default() += 1;
+        }
+    }
+    letters
+}
+
 fn merge_counters(result: &mut HashMap<char, usize>, data: &HashMap<char, usize>) {
     for (new_char, new_counter) in data {
-        let counter = result.get_mut(&new_char);
-        if let Some(counter) = counter {
-            *counter += new_counter;
-        } else {
-            result.insert(*new_char, *new_counter);
-        }
+        *result.entry(*new_char).or_default() += new_counter;
     }
 }
